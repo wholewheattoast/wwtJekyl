45e494b2cfa775418df6eb0ab009f9acbd4d5d20
shawn eisenach <shawn@wholewheattoast.com>
2013-04-10 22:28:23 -0700

    i guess a new init commit

--- scripts/sketchbook-page-maker.py
+++ scripts/sketchbook-page-maker.py
@@ -5,9 +5,11 @@ import os
 #name and path
 working_directory_path = "/Users/shawn/Dropbox/Sites/wwtJekyl/_posts/sketchbook/"
 output_directory_path = "."
+output_domain = "http://www.wholewheattoast.com/"
 sketchbook_name = raw_input('Enter sketchbook name: ')
 title_string_prepend = """title: """ + str(sketchbook_name)
 directory = working_directory_path + sketchbook_name
+
 
 #pages
 pagesInput = raw_input('How many pages? ')
@@ -27,7 +29,7 @@ img_classes = """class="img-rounded" """
 ## init
 counter = 1
 
-def write_to_file(name_of, title_string, next_page_url, prev_page_url, img_url):
+def write_to_file(name_of, title_string, next_page_url, next_page_meta, prev_page_meta, img_url):
 	f = open(name_of, 'w+')
 	f.write("---")
 	f.write('\n')
@@ -39,15 +41,15 @@ def write_to_file(name_of, title_string, next_page_url, prev_page_url, img_url):
 	f.write('\n')
 	f.write("tags: sketchbook, moleskine")
 	f.write('\n')
-	f.write(next_page_url)
+	f.write(next_page_meta)
 	f.write('\n')
-	f.write(prev_page_url)
+	f.write(prev_page_meta)
 	f.write('\n')
 	f.write("---")
 	f.write('\n')
-	f.write("<p>")
+	f.write('<p class="sketchbook_container">')
 	f.write('\n')
-	f.write(str(img_url))
+	f.write('<a href="' + next_page_url + '">' + img_url + '</a>')
 	f.write('\n')
 	f.write("</p>")
 	f.write('\n')
@@ -61,46 +63,51 @@ if not os.path.exists(directory):
 while counter == 1:
 	img_url = """<img """ + img_classes + 'src="' + output_directory_path + """/image/""" + sketchbook_name + """-front-cover.jpg" alt=" """ + sketchbook_name + """ sketchbook front cover" """ + img_width + " " + img_height + """>"""
 	name_of = working_directory_path + sketchbook_name + "/" + sketchbook_name + "-front-cover" + ".html"
-	next_page_url = """next-page: """ + output_directory_path + "/" + sketchbook_name + """-pp-""" + str(left_page) + """-""" + str(right_page) +  """.html"""
-	prev_page_url = """prev-page: """ + output_directory_path + "/" + sketchbook_name + "/index" + """.html"""
+	next_page_meta = """next-page: """ + output_directory_path + "/" + sketchbook_name + """-pp-""" + str(left_page) + """-""" + str(right_page) +  """.html"""
+	next_page_url = sketchbook_name + """-pp-""" + str(left_page) + """-""" + str(right_page) +  """.html"""
+	prev_page_meta = """prev-page: """ + output_domain + "/sketchbooks.html"
 	title_string = title_string_prepend + " " + "Front Cover"
-	front_cover = write_to_file(name_of, title_string, next_page_url, prev_page_url, img_url)
+	front_cover = write_to_file(name_of, title_string, next_page_url, next_page_meta, prev_page_meta, img_url)
 	counter += 1
 while counter == 2:
 	img_url = """<img """ + img_classes + 'src="' + output_directory_path + """/image/""" + sketchbook_name + """-pp-""" + str(left_page) + "-" + str(right_page) + """.jpg" alt=" """ + sketchbook_name + """ sketchbook """ + str(left_page) + """-""" + str(right_page) + '" ' + img_width + " " + img_height + """>"""
-	next_page_url = """next-page: """ + output_directory_path + "/" + sketchbook_name + """-pp-""" + str((left_page + 2)) + """-""" + str((right_page + 2)) +  """.html"""
-	prev_page_url = """prev-page: """ + output_directory_path + "/" + sketchbook_name + "-front-cover" + ".html"
+	next_page_url = sketchbook_name + """-pp-""" + str((left_page + 2)) + """-""" + str((right_page + 2)) +  """.html"""
+	next_page_meta = """next-page: """ + output_directory_path + "/" + sketchbook_name + """-pp-""" + str((left_page + 2)) + """-""" + str((right_page + 2)) +  """.html"""
+	prev_page_meta = """prev-page: """ + output_directory_path + "/" + sketchbook_name + "-front-cover" + ".html"
 	name_of = working_directory_path + sketchbook_name + "/" + sketchbook_name + "-pp-" + str(left_page) + "-" + str(right_page) + ".html"
 	title_string = title_string_prepend + " " + "pp " + str(left_page) + """-""" + str(right_page)
-	body_pages = write_to_file(name_of, title_string, next_page_url, prev_page_url, img_url)
+	body_pages = write_to_file(name_of, title_string, next_page_url, next_page_meta, prev_page_meta, img_url)
 	left_page += 2
 	right_page += 2
 	counter += 1
 while counter > 2 and counter <= (spreads):
 	img_url = """<img """ + img_classes + 'src="' + output_directory_path + """/image/""" + sketchbook_name + """-pp-""" + str(left_page) + "-" + str(right_page) + """.jpg" alt=" """ + sketchbook_name + """ sketchbook """ + str(left_page) + """-""" + str(right_page) + '" ' + img_width + " " + img_height + """>"""
-	next_page_url = """next-page: """ + output_directory_path + "/" + sketchbook_name + """-pp-""" + str((left_page + 2)) + """-""" + str((right_page + 2)) +  """.html"""
-	prev_page_url = """prev-page: """ + output_directory_path + "/" + sketchbook_name + """-pp-""" + str((left_page - 2)) + """-""" + str((right_page - 2)) +  """.html"""
+	next_page_url = sketchbook_name + """-pp-""" + str((left_page + 2)) + """-""" + str((right_page + 2)) +  """.html"""
+	next_page_meta = """next-page: """ + output_directory_path + "/" + sketchbook_name + """-pp-""" + str((left_page + 2)) + """-""" + str((right_page + 2)) +  """.html"""
+	prev_page_meta = """prev-page: """ + output_directory_path + "/" + sketchbook_name + """-pp-""" + str((left_page - 2)) + """-""" + str((right_page - 2)) +  """.html"""
 	name_of = working_directory_path + sketchbook_name + "/" + sketchbook_name + "-pp-" + str(left_page) + "-" + str(right_page) + ".html"
 	title_string = title_string_prepend + " " + "pp " + str(left_page) + """-""" + str(right_page)
-	body_pages = write_to_file(name_of, title_string, next_page_url, prev_page_url, img_url)
+	body_pages = write_to_file(name_of, title_string, next_page_url, next_page_meta, prev_page_meta, img_url)
 	left_page += 2
 	right_page += 2
 	counter += 1
 while counter == (spreads + 1):
 	img_url = """<img """ + img_classes + 'src="' + output_directory_path + """/image/""" + sketchbook_name + """-pp-""" + str(left_page) + "-" + str(right_page) + """.jpg" alt=" """ + sketchbook_name + """ sketchbook """ + str(left_page) + """-""" + str(right_page) + '" ' + img_width + " " + img_height + """>"""
-	next_page_url = """next-page: """ + output_directory_path + "/" + sketchbook_name + "-back-cover" + ".html"
-	prev_page_url = """prev-page: """ + output_directory_path + "/" + sketchbook_name + """-pp-""" + str((left_page - 2)) + """-""" + str((right_page - 2)) +  """.html"""
+	next_page_url = sketchbook_name + "-back-cover" + ".html"
+	next_page_meta = """next-page: """ + output_directory_path + "/" + sketchbook_name + "-back-cover" + ".html"
+	prev_page_meta = """prev-page: """ + output_directory_path + "/" + sketchbook_name + """-pp-""" + str((left_page - 2)) + """-""" + str((right_page - 2)) +  """.html"""
 	name_of = working_directory_path + sketchbook_name + "/" + sketchbook_name + "-pp-" + str(left_page) + "-" + str(right_page) + ".html"
 	title_string = title_string_prepend + " " + "pp " + str(left_page) + """-""" + str(right_page)
-	body_pages = write_to_file(name_of, title_string, next_page_url, prev_page_url, img_url)
+	body_pages = write_to_file(name_of, title_string, next_page_url, next_page_meta, prev_page_meta, img_url)
 	counter += 1
 while counter == (spreads + 2):
 	img_url = """<img """ + img_classes + 'src="' + output_directory_path + """/image/""" + sketchbook_name + """-back-cover.jpg" alt=" """ + sketchbook_name + """ sketchbook back cover" """ + img_width +" " + img_height + """>"""
 	name_of = working_directory_path + sketchbook_name + "/" + sketchbook_name + "-back-cover" + ".html"
-	next_page_url = """next-page: """ + output_directory_path + "/" + sketchbook_name + "/index" + """.html"""
-	prev_page_url = """prev-page: """ + output_directory_path + "/" + sketchbook_name + """-pp-""" + str(left_page) + """-""" + str(right_page) +  """.html"""
+	next_page_url = sketchbook_name + "-front-cover" + ".html"
+	next_page_meta = """next-page: """ + output_directory_path + "/" + sketchbook_name + "-front-cover" + ".html"
+	prev_page_meta = """prev-page: """ + output_directory_path + "/" + sketchbook_name + """-pp-""" + str(left_page) + """-""" + str(right_page) +  """.html"""
 	title_string = title_string_prepend + " " + "Back Cover"
-	back_cover = write_to_file(name_of, title_string, next_page_url, prev_page_url, img_url)
+	back_cover = write_to_file(name_of, title_string, next_page_url, next_page_meta, prev_page_meta, img_url)
 	counter +=1
 else:
 	print "all done with " + sketchbook_name
---
+++ scripts/test.py
@@ -0,0 +1,1 @@
+raw_input('Enter your input:')
