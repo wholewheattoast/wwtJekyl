5f44166024604d367ea10147300433a865f526ef
wholewheattoast <wholewheattoast@gmail.com>
2015-01-10 23:11:11 -0800

    In progress commit.

--- scripts/make_sketchbook.py
+++ scripts/make_sketchbook.py
@@ -2,6 +2,7 @@ import argparse
 import pystache
 import os
 import time
+import toast_tools
 
 import pdb
 #pdb.set_trace()
@@ -31,12 +32,14 @@ for root, dirs, files in os.walk(sb_image_dir):
                 pass
             elif name == "{}-front-cover-icon.jpg".format(sb_url_safe_name):
                 pass
-            #egyptian_bun_cover_icon
             elif name == "{}_cover_icon.jpg".format(sb_url_safe_name.replace("-", "_")):
                 pass
             else:
-                striped_name = name.replace(".jpg", "")
-                sb_image_files.append(striped_name)
+                strip_ext = name.replace(".jpg", "")
+                strip_base = strip_ext.replace(sb_url_safe_name, "")
+                
+                sb_numbers_only = strip_base.replace("-pp-", "")
+                sb_image_files.append(sb_numbers_only)
 
 print len(sb_image_files)
 print ""
@@ -46,7 +49,7 @@ print ""
 sorted_image_list = sorted(sb_image_files)
 print sorted_image_list
 
-# build directory
+# create sb dir
 sb_directory = os.path.dirname("../sketchbooks/{}".format(sb_url_safe_name))
 
 if not os.path.exists(sb_directory):
@@ -54,4 +57,46 @@ if not os.path.exists(sb_directory):
     
     
 # build pages
-# build index
+# Iterate through and build a dictionary
+# make it json
+# render with pystache, keep the template out of here
+# one template for pages, one for index
+# may only need a single dict
+
+sb_dict = {}
+
+spreads_list = []
+
+for i in sorted_image_list:
+    temp_spread_dict = {}
+    
+    i_without_dash = i.format("-", " ")
+    i_split = i.split()
+    
+    temp_spread_dict["spread_name"] = "{}-pp-{}".format(sb_url_safe_name,i)
+    # need to be able to deal w fc and bc images here
+    temp_spread_dict["left_page_number"] = i_split[0]
+    temp_spread_dict["right_page_numer"] = i_split[1]
+#    temp_spread_dict["prev_spread"] = ?
+#    temp_spread_dict["next_spread"] = ?
+    spreads_list.append(temp_spread_dict)
+
+# Flesh out sb_dict
+
+sb_dict["sb_name"] = sb_url_safe_name
+sb_dict["sb_spreads"] = spreads_list
+sb_dict["page_count"] = "?"
+#sb_dict["sb_date_started"] = "?"
+#sb_dict["sb_date_ended"] = "?"
+#sb_dict["sb_dimensions"] = "?"
+
+
+# generate index
+
+# write_out_template(dictionary, path, file_name, template)
+
+sb_index_template = "../_templates/sb_index.mustache"
+
+toast_tools.write_out_template(sb_dict, sb_directory, index, sb_index_template)
+
+
---
+++ scripts/toast_tools.py
@@ -0,0 +1,28 @@
+import argparse
+import json
+import os
+import os.path
+import pystache
+import time
+import uuid
+
+
+import pdb
+
+def write_out_template(dictionary, path, file_name, template):
+    """
+        Read in json
+        Render the dictionary using the given template
+        Save the file with file_name
+        to the location specified by the path
+    """
+
+    html_path = "{}/{}".format(path, file_name)
+    html_file = open(html_path,"w")
+
+    results_template = open("../_templates/{}.mustache".format(template)).read()
+
+    html_results = pystache.render(results_template, dictionary)
+    html_file.write(html_results)
+
+    html_file.close()
