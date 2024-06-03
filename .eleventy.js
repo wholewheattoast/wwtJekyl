94a1f94f51de079f04feea3e25b989d4bb1fb325
Shawn Eisenach <shawn@wholewheattoast.com>
2024-06-02 20:49:00 -0700

    Add image processing plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,4 +1,7 @@
 const pluginRss = require("@11ty/eleventy-plugin-rss");
+// consider for update after ver 3
+// const { eleventyImageTransformPlugin } = require("@11ty/eleventy-img");
+const Image = require("@11ty/eleventy-img");
 
 module.exports = function(eleventyConfig) {
   const { DateTime } = require("luxon");
@@ -8,12 +11,32 @@ module.exports = function(eleventyConfig) {
       closingSingleTag: "default" // opt-out of <img/>-style XHTML single tags
     }
   });
-  
+  // Only one module.exports per configuration file, please!
+  eleventyConfig.addShortcode("image", async function (src, alt, sizes) {
+    let metadata = await Image(src, {
+      widths: [368, 736, 900],
+      formats: ["webp", "jpeg"],
+      urlPath: "/image-processed/",
+      outputDir: "./image-processed/",
+    });
+
+    let imageAttributes = {
+      alt,
+      sizes,
+      loading: "lazy",
+      decoding: "async",
+    };
+
+    // You bet we throw an error on a missing alt (alt="" works okay)
+    return Image.generateHTML(metadata, imageAttributes);
+  });
+
   eleventyConfig.addLiquidFilter("dateToRfc3339", pluginRss.dateToRfc3339);
   eleventyConfig.addLiquidFilter("dateToRfc822", pluginRss.dateToRfc822);
-  
+
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("image");
+  eleventyConfig.addPassthroughCopy("image-processed");
   eleventyConfig.addPassthroughCopy("tumblr_files");
   eleventyConfig.addPassthroughCopy("keybase.txt");
   
@@ -42,4 +65,3 @@ module.exports = function(eleventyConfig) {
     return tagsList
   })
 };
-
